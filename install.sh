3b7968a10e58d6c86e15a5967ade287f5f4f02a5
antonlee2013 <antonlee2013@163.com>
2016-04-04 17:46:50 +0800

    change i3status to i3conky

--- install.sh
+++ install.sh
@@ -1,8 +1,6 @@
 #! /bin/bash
 # Name : install.sh
 # Func : 
-# Time : 2016-04-01 09:00:52
+# Time : 2016-04-04 17:42:56
 
-ln -sf ~/.i3/config_work_laptop ~/.i3/config
-ln -sf ~/.i3/i3status.conf ~/.i3status.conf
-
+test ! -L $HOME/.i3/config && ln -sf $HOME/.i3/conkyrc-i3bar $HOME/.i3/config
