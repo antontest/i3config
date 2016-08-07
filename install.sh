101e58a6807e9c6f9d5c7bf6c5b4dc4003412411
Antonio <antonlee2013@163.com>
2016-08-07 14:37:26 +0800

    i3wm: add shell script toggle_touchpad.

--- install.sh
+++ install.sh
@@ -4,3 +4,4 @@
 # Time : 2016-04-04 17:42:56
 
 test ! -L $HOME/.i3/config && ln -sf $HOME/.i3/i3wm_config $HOME/.i3/config
+test ! -L $HOME/.compton.conf && test ! -L $HOME/.i3/compton/compton.conf && ln -sf $HOME/.i3/compton/compton.conf $HOME/.compton.conf
